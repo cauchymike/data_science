978ef64c36333a345e83f7046e34a1fe33599759
cauchymike <59985204+cauchymike@users.noreply.github.com>
2020-09-25 17:02:10 -0600

    Update forms.py

--- mathsclinicblog/blogpost/forms.py
+++ mathsclinicblog/blogpost/forms.py
@@ -2,8 +2,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField,TextAreaField
 from wtforms.validators import DataRequired
+from flask_pagedown.fields import PageDownField
 
 class BlogPostForm(FlaskForm):
     title = StringField('Title',validators=[DataRequired()])
-    text = TextAreaField('Text',validators=[DataRequired()])
+    text = PageDownField('Text',validators=[DataRequired()])
     submit = SubmitField("Post")
